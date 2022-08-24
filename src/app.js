341f1de6f841b2d5c4f4c9fbb721b6c0efdbae73
markWIMPER <cc947331424@icloud.com>
2022-08-24 22:49:15 +0800

    使用vue模版的方式进行改写

---
+++ src/app.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Mark
+ * @Date: 2022-08-24 22:38:50
+ * @Description: app 主入口文件
+ */
+import Vue from 'vue';
+
+new Vue({
+	el: '#aapp'
+});
---
+++ src/button.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<button class="m-button">Mark</button>
+	</div>
+</template>
+<script>
+export default {
+	name: '',
+	template: `
+    <button class="m-button">Mark</button>
+  `
+};
+</script>
+<style lang="scss">
+.m-button {
+	font-size: var(--font-size);
+	padding: 0 1em;
+	/*width*/
+	border-radius: var(--border-radius);
+	border: 1px solid var(--border-color);
+	font: inherit;
+	height: var(--button-height);
+	background: var(--button-bg);
+	&:hover {
+		border-color: var(--border-color-hover);
+	}
+
+	&:active {
+		background-color: var(--button-active-bg);
+	}
+
+	&:focus {
+		outline: none;
+	}
+}
+</style>
