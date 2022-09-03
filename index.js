22791b4558a6580384aac52ac5d4504bd28135b7
markWIMPER <cc947331424@icloud.com>
2022-09-03 13:25:25 +0800

    # 更新名称

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Mark
+ * @Date: 2022-09-03 12:30:12
+ * @Description:
+ */
+import Button from './src/component/button.vue';
+import ButtonGroup from './src/component/button-group';
+import Icon from './src/component/icon';
+
+export { Button, ButtonGroup, Icon };
